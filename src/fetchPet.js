fdd940bbeaae38d97b89fe15cb62e306b04f7ab0
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-07-20 22:19:54 +0100

    Added fetchBreedList, fetchPet and fetchSearch

---
+++ src/fetchPet.js
@@ -0,0 +1,11 @@
+export default async function fetchPet({ queryKey }) {
+  const id = queryKey[1];
+
+  const apiResponse = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+
+  if (!apiResponse.ok) {
+    throw new Error(`details/${id} fetch not ok`);
+  }
+
+  return apiResponse.json();
+}
