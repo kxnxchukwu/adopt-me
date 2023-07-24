164ae236438b243e984d19324ea2c20bb56e87c2
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-07-24 09:33:32 +0100

    Lazy loaded SearchParams and Details components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AdoptedPetContext from "./AdoptedPetContext";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
+
+const SearchParams = lazy(() => import("./SearchParams"));
+const Details = lazy(() => import("./Details"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -27,15 +28,26 @@ const App = () => {
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
           <AdoptedPetContext.Provider value={adoptedPet}>
-            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
-              <Link className="text-6xl text-white hover:text-gray-200" to="/">
-                Adopt Me
-              </Link>
-            </header>
-            <Routes>
-              <Route path="/details/:id" element={<Details />} />{" "}
-              <Route path="/" element={<SearchParams />} />
-            </Routes>
+            <Suspense
+              fallback={
+                <div className="loading-pane">
+                  <h2 className="loader">🌀</h2>
+                </div>
+              }
+            >
+              <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+                <Link
+                  className="text-6xl text-white hover:text-gray-200"
+                  to="/"
+                >
+                  Adopt Me
+                </Link>
+              </header>
+              <Routes>
+                <Route path="/details/:id" element={<Details />} />{" "}
+                <Route path="/" element={<SearchParams />} />
+              </Routes>
+            </Suspense>
           </AdoptedPetContext.Provider>
         </QueryClientProvider>
       </BrowserRouter>
