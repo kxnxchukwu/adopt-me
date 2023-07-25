7b6f493cc9db96e6f9d2653d582a8505e3a89eca
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-07-25 11:37:06 +0100

    Converted fetchSearch to TypeScript

---
+++ src/fetchSearch.ts
@@ -0,0 +1,26 @@
+import { QueryFunction } from '@tanstack/react-query';
+import { Animal, PetAPIResponse } from './APIResponseTypes';
+
+const fetchSearch: QueryFunction<
+  PetAPIResponse,
+  [
+    'search',
+    {
+      location: string;
+      animal: Animal;
+      breed: string;
+    }
+  ]
+> = async ({ queryKey }) => {
+  const { animal, location, breed } = queryKey[1];
+  const response = await fetch(
+    `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+  );
+
+  if (!response.ok)
+    throw new Error(`pet search not okay: ${animal}, ${location}, ${breed}`);
+
+  return response.json();
+};
+
+export default fetchSearch;
