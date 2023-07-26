dcd094f79175a8f8a0d5b80ce73c8afeeb354448
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-07-26 10:32:51 +0100

    Added petApiService

---
+++ src/petApiService.ts
@@ -0,0 +1,45 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import {
+  Animal,
+  BreedAPIResponse,
+  Pet,
+  PetAPIResponse
+} from './APIResponseTypes';
+
+export interface GetPetArgs {
+  id: string;
+}
+
+export interface GetBreedsArgs {
+  animal: Animal;
+}
+
+export interface SearchArgs {
+  animal: Animal;
+  location: string;
+  breed: string;
+}
+
+export const petApi = createApi({
+  reducerPath: 'petApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://pets-v2.dev-apis.com' }),
+  endpoints: (builder) => ({
+    getPet: builder.query<Pet, GetPetArgs>({
+      query: ({ id }) => ({ url: 'pets', params: { id } }),
+      transformResponse: (response: PetAPIResponse) => response.pets[0]
+    }),
+    getBreeds: builder.query<string[], GetBreedsArgs>({
+      query: ({ animal }) => ({ url: 'breeds', params: { animal } }),
+      transformResponse: (response: BreedAPIResponse) => response.breeds
+    }),
+    search: builder.query<Pet[], SearchArgs>({
+      query: ({ animal, location, breed }) => ({
+        url: 'pets',
+        params: { animal, location, breed }
+      }),
+      transformResponse: (response: PetAPIResponse) => response.pets
+    })
+  })
+});
+
+export const { useGetPetQuery, useGetBreedsQuery, useSearchQuery } = petApi;
