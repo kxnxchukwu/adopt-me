c3cd3d7c11ec5dad6a0296e6201a3c06e208823b
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-08-10 18:17:24 +0100

    Updated Pet Component to make it testable

--- src/Pet.tsx
+++ src/Pet.tsx
@@ -20,14 +20,14 @@ export default function Pet({
 }: PetProps) {
   let hero = 'http://pets-images.dev-apis.com/pets/none.jpg';
 
-  if (images.length) {
+  if (images?.length) {
     hero = images[0];
   }
 
   return (
     <Link to={`/details/${id}`} className="relative block">
       <div className="image-container">
-        <img src={hero} alt={name} />
+        <img data-testid="thumbnail" src={hero} alt={name} />
       </div>
       <div className="absolute bottom-0 left-0 bg-gradient-to-tr from-white to-transparent pr-2 pt-2">
         <h1>{name}</h1>
