30a1fa30c89514dac8e62b3e92cc641df4484379
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-08-11 22:36:02 +0100

    Update vite config js file

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,8 @@ export default defineConfig({
   test: {
     environment: 'happy-dom'
   },
-  setupFiles: ['./setupVitest.ts']
+  setupFiles: ['./setupVitest.ts'],
+  build: {
+    outDir: '../dist'
+  }
 });
