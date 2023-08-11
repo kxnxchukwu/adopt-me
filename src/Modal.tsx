15f4cda629d1486da0baf9fd545fe1ffc9fb5315
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-08-11 22:31:18 +0100

    Update Modal Component

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,15 +1,15 @@
-import { useEffect, MutableRefObject, useRef, ReactElement } from "react";
-import { createPortal } from "react-dom";
+import { useEffect, MutableRefObject, useRef, ReactElement } from 'react';
+import { createPortal } from 'react-dom';
 
 export default function Modal({ children }: { children: ReactElement }) {
   const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
 
   if (!elRef.current) {
-    elRef.current = document.createElement("div");
+    elRef.current = document.createElement('div');
   }
 
   useEffect(() => {
-    const modalRoot = document.getElementById("modal");
+    const modalRoot = document.getElementById('modal');
     if (!modalRoot || !elRef.current) {
       return;
     }
@@ -22,5 +22,10 @@ export default function Modal({ children }: { children: ReactElement }) {
     };
   }, []);
 
-  return createPortal(<div>{children}</div>, elRef.current);
+  return createPortal(
+    <div className="fixed top-12 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+      {children}
+    </div>,
+    elRef.current
+  );
 }
